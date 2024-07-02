c2c7d0c2ada54e7e134004d99511943c7fa52428
rameem2003 <rameem2019@gmail.com>
2024-07-02 10:50:39 +0600

    update

--- src/components/StudentDetails.jsx
+++ src/components/StudentDetails.jsx
@@ -1,4 +1,4 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Title from './Title';
 
@@ -7,70 +7,160 @@ const StudentDetails = ({route, navigation}) => {
 
   console.log(data);
   return (
-    <View>
+    <View style={{paddingBottom: '20%'}}>
       <Title title={data.studentNameEnglish} />
-      <View style={styles.containt}>
-        <Image
-          style={{borderRadius: 8, marginHorizontal: 'auto', marginBottom: 20}}
-          source={{uri: data.studentImage}}
-          height={250}
-          width={250}
-        />
-        <Text style={styles.name}>Board Roll :{data.diplomaBoardRoll}</Text>
-        <Text style={styles.name}>Registration No :{data.diplomaBoardReg}</Text>
+      <ScrollView>
+        <View style={styles.containt}>
+          <Image
+            style={{
+              borderRadius: 8,
+              marginHorizontal: 'auto',
+              marginBottom: 20,
+            }}
+            source={{uri: data.studentImage}}
+            height={250}
+            width={250}
+          />
+          <Text
+            style={{
+              ...styles.name,
+              fontSize: 30,
+              textTransform: 'capitalize',
+              color: 'red',
+            }}>
+            {data.department} Department
+          </Text>
+          <Text style={styles.name}>Board Roll :{data.diplomaBoardRoll}</Text>
+          <Text style={styles.name}>
+            Registration No :{data.diplomaBoardReg}
+          </Text>
 
-        <View
-          style={{
-            marginTop: 20,
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            gap: 10,
-          }}>
           <View
             style={{
-              width: '50%',
-              height: 100,
-              padding: 10,
-              borderRadius: 8,
-              backgroundColor: '#1E5391',
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              gap: 10,
+              marginBottom: 20,
             }}>
-            <Text style={{color: 'white', textAlign: 'center', fontSize: 40}}>
-              {data.bloodGroup}
-            </Text>
-            <Text style={{color: 'white', textAlign: 'center', fontSize: 20}}>
-              Blood
-            </Text>
+            <View
+              style={{
+                width: '50%',
+                height: 100,
+                padding: 10,
+                borderRadius: 8,
+                backgroundColor: '#1E5391',
+              }}>
+              <Text style={{color: 'white', textAlign: 'center', fontSize: 40}}>
+                {data.bloodGroup}
+              </Text>
+              <Text style={{color: 'white', textAlign: 'center', fontSize: 20}}>
+                Blood
+              </Text>
+            </View>
+            <View
+              style={{
+                //   flex: 1,
+                width: '50%',
+                height: 100,
+                padding: 10,
+                borderRadius: 8,
+                backgroundColor: '#1E5391',
+              }}>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  fontSize: 40,
+                  textTransform: 'capitalize',
+                }}>
+                {data.gender}
+              </Text>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  fontSize: 20,
+                }}>
+                Gender
+              </Text>
+            </View>
           </View>
+
           <View
             style={{
-              //   flex: 1,
-              width: '50%',
-              height: 100,
-              padding: 10,
-              borderRadius: 8,
-              backgroundColor: '#1E5391',
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              gap: 10,
+              marginBottom: 20,
             }}>
-            <Text
+            <View
               style={{
-                color: 'white',
-                textAlign: 'center',
-                fontSize: 40,
-                textTransform: 'capitalize',
+                width: '50%',
+                height: 100,
+                padding: 10,
+                borderRadius: 8,
+                backgroundColor: '#1E5391',
               }}>
-              {data.gender}
-            </Text>
-            <Text
+              <Text style={{color: 'white', textAlign: 'center', fontSize: 40}}>
+                {data.sscResult}
+              </Text>
+              <Text style={{color: 'white', textAlign: 'center', fontSize: 20}}>
+                SSC GPA
+              </Text>
+            </View>
+            <View
               style={{
-                color: 'white',
-                textAlign: 'center',
-                fontSize: 20,
+                //   flex: 1,
+                width: '50%',
+                height: 100,
+                padding: 10,
+                borderRadius: 8,
+                backgroundColor: '#1E5391',
               }}>
-              Gender
-            </Text>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  fontSize: 40,
+                  textTransform: 'capitalize',
+                }}>
+                {data.sscGroup}
+              </Text>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  fontSize: 20,
+                }}>
+                SSC Group
+              </Text>
+            </View>
           </View>
+
+          <Text
+            style={{
+              ...styles.name,
+              fontSize: 20,
+            }}>
+            Basic Info
+          </Text>
+
+          <Text style={{...styles.name, textAlign: 'left', color: 'black'}}>
+            Father : {data.fathersName}
+          </Text>
+          <Text style={{...styles.name, textAlign: 'left', color: 'black'}}>
+            Mother : {data.mothersName}
+          </Text>
+          <Text style={{...styles.name, textAlign: 'left', color: 'black'}}>
+            Date of Birth : {data.dob}
+          </Text>
+          <Text style={{...styles.name, textAlign: 'left', color: 'black'}}>
+            Email : {data.email}
+          </Text>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
